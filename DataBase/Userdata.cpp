e700af235c43e8ef55340db1debe36d422872c22
Kimc17 <31198587+Kimc17@users.noreply.github.com>
2018-05-28 23:41:41 -0600

    10

---
+++ DataBase/Userdata.cpp
@@ -0,0 +1,57 @@
+//
+// Created by karina on 27/05/18.
+//
+
+#include "Userdata.h"
+
+ std::string Userdata::getUsername()  {
+    return username;
+}
+std::string Userdata::getNombre()  {
+    return nombre;
+}
+
+
+void Userdata::setUsername( std::string username) {
+    Userdata::username = username;
+}
+void Userdata::setNombre( std::string nombre) {
+    Userdata::nombre = nombre;
+}
+
+ std::string Userdata::getPassword()  {
+    return password;
+}
+
+void Userdata::setPassword( std::string password) {
+    Userdata::password = password;
+}
+
+
+void Userdata::setPreferences( SimpleLinkedList<std::string> preferences) {
+    Userdata::preferences = preferences;
+}
+
+void Userdata::setFriends(SimpleLinkedList<std::string> friends) {
+    Userdata::friends = friends;
+}
+
+Userdata::Userdata() {
+    this->username = "";
+    this->password = "";
+}
+
+Userdata::Userdata(std::string username, std::string password, SimpleLinkedList<std::string> friends, SimpleLinkedList<std::string> preferences) {
+    this->username = username;
+    this->password = password;
+    this->friends = friends;
+    this->preferences = preferences;
+}
+
+SimpleLinkedList<std::string> Userdata::getFriends(){
+    return friends;
+}
+
+SimpleLinkedList< std::string> Userdata::getPreferences()  {
+    return preferences;
+}
---
+++ DataBase/Userdata.h
@@ -0,0 +1,50 @@
+//
+// Created by karina on 27/05/18.
+//
+
+#ifndef SERVERDATABASE_USERDATA_H
+#define SERVERDATABASE_USERDATA_H
+
+
+#include <string>
+#include "../DataStructures/SimpleLinkedList.h"
+
+class Userdata {
+
+public:
+     SimpleLinkedList<std::string> getFriends() ;
+
+     SimpleLinkedList<std::string> getPreferences() ;
+
+    Userdata();
+
+    Userdata(std::string username, std::string password, SimpleLinkedList<std::string> friends, SimpleLinkedList<std::string>preferences);
+
+    std::string getUsername();
+
+    void setUsername(std::string username);
+    void setNombre(std::string username);
+
+    std::string getPassword();
+    std::string getNombre();
+
+    void setPassword(std::string password);
+
+
+
+    void setFriends(SimpleLinkedList<std::string> friends);
+
+
+
+    void setPreferences(SimpleLinkedList<std::string> preferences);
+
+private:
+    std::string username,password, nombre;
+
+    SimpleLinkedList<std::string> preferences, friends;
+
+
+};
+
+
+#endif //SERVERDATABASE_USERDATA_H
